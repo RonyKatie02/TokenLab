3092194f3f1fc0a1481bcab221c6324f65facdae
RonyKatie02 <104327213+RonyKatie02@users.noreply.github.com>
2025-07-15 21:03:58 +0000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Token Launcher SPL</h1>
+      <p>Bienvenue ! Ce site permettra de créer des tokens SPL sur Solana.</p>
+    </div>
+  );
+}
+
+export default App;
